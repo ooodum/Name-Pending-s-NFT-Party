7f1f1924e7de6acb6cb72eed4e37f3ded8cca5fa
Oodum <adamtam2004@outlook.com>
2022-09-26 13:52:55 -0400

    Dice mechanic added

--- BGP[Proto1]/Assets/Scripts/PlayerController.cs
+++ BGP[Proto1]/Assets/Scripts/PlayerController.cs
@@ -8,14 +8,37 @@ public class PlayerController : MonoBehaviour
     public GameObject leftTri;
     public GameObject rightTri;
     public GameObject downTri;
+
+
     private string lastPress;
-    private Vector3 vel;
-    void Start() { 
+
+    private int turnPhase = 1;
+    private int diceRoll;
+    void Start() {
+        print("Press space to roll die");
     }
 
     void Update(){
+        if (turnPhase == 1) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                diceRoll = Random.Range(1, 299);
+                Debug.Log(diceRoll);
+                turnPhase++;
+            }
+        }
+        if (turnPhase == 2) {
+            if (diceRoll > 0) {
+                movePlayer();
+            } else if(diceRoll == 0) {
+                print("no more turns, exiting");
+                turnPhase++;
+            }
+        }
+    }
+
+    void movePlayer() {
         // Arrow/WASD Control
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && upTri.GetComponent<SpriteRenderer>().enabled) {
             //If the pressed key is not opposite of last direction
             if (lastPress != "down") {
                 //Set all but the opposite-side-triangle active
@@ -23,16 +46,18 @@ public class PlayerController : MonoBehaviour
                 leftTri.SetActive(true);
                 rightTri.SetActive(true);
                 downTri.SetActive(false);
-
                 //If the player is allowed to move in the direction, move one unit
                 if (upTri.GetComponent<SpriteRenderer>().enabled) {
                     transform.Translate(0, 1, 0);
+                    //Dice decrement
+                    diceRoll--;
+                    print(diceRoll);
                 }
                 //Set the lastPress variable to the current direction
                 lastPress = "up";
             } else print("Cannot move up"); //Debug
         }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) && leftTri.GetComponent<SpriteRenderer>().enabled) {
             if (lastPress != "right") {
                 upTri.SetActive(true);
                 leftTri.SetActive(true);
@@ -41,11 +66,13 @@ public class PlayerController : MonoBehaviour
 
                 if (leftTri.GetComponent<SpriteRenderer>().enabled) {
                     transform.Translate(-1, 0, 0);
+                    diceRoll--;
+                    print(diceRoll);
                 }
                 lastPress = "left";
             } else print("Cannot move left");
         }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) && downTri.GetComponent<SpriteRenderer>().enabled) {
             if (lastPress != "up") {
                 upTri.SetActive(false);
                 leftTri.SetActive(true);
@@ -54,11 +81,13 @@ public class PlayerController : MonoBehaviour
 
                 if (downTri.GetComponent<SpriteRenderer>().enabled) {
                     transform.Translate(0, -1, 0);
+                    diceRoll--;
+                    print(diceRoll);
                 }
                 lastPress = "down";
             } else print("Cannot move down");
         }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) && rightTri.GetComponent<SpriteRenderer>().enabled) {
             if (lastPress != "left") {
                 upTri.SetActive(true);
                 leftTri.SetActive(false);
@@ -67,6 +96,8 @@ public class PlayerController : MonoBehaviour
 
                 if (rightTri.GetComponent<SpriteRenderer>().enabled) {
                     transform.Translate(1, 0, 0);
+                    diceRoll--;
+                    print(diceRoll);
                 }
                 lastPress = "right";
             } else print("Cannot move right");
