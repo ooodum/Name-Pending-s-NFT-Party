2932f1b7713fcb171a4ff0d4bf35886ae11c1779
Oodum <adamtam2004@outlook.com>
2022-09-26 21:02:52 -0400

    Smooth Move Implemented

--- BGP[Proto1]/Assets/Scripts/AdjacentCheck.cs
+++ BGP[Proto1]/Assets/Scripts/AdjacentCheck.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class AdjacentCheck : MonoBehaviour
 {
     private bool touching;
-
+    public GameObject tile;
     private void OnTriggerStay2D(Collider2D collision) {
         touching = true;
+        tile = collision.gameObject;
     }
     private void OnTriggerExit2D(Collider2D collision) {
         touching = false;
--- BGP[Proto1]/Assets/Scripts/PlayerController.cs
+++ BGP[Proto1]/Assets/Scripts/PlayerController.cs
@@ -4,41 +4,67 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Reference each of the four triangles surrounding the player
     public GameObject upTri;
     public GameObject leftTri;
     public GameObject rightTri;
     public GameObject downTri;
 
-
+    //The last pressed key (that was a "valid" move)
     private string lastPress;
 
+    //Defines which phase of the turn the player is on (Rolling dice, moving, etc.)
     private int turnPhase = 1;
+
+    //Says whether or not the player is allowed to move
+    private bool canMove = true;
+
+    //The value of the dice roll
     private int diceRoll;
+
+    //The position where the player will smoothly move to (SD stands for SmoothDamp)
+    private Vector3 SDPos;
+
+    //Says whether or not the SmoothDamp is on or off (for performance purposes)
+    private bool SDCheck = false;
+
+    //Move speed of SmoothDamp. Lower is faster.
+    private float moveSpeed = 10f;
+
+    //Used for SmoothDamp
+    private Vector2 vel = Vector2.zero;
+
+    //A value that is compared against the magnitude of the position of the player and SDPos (for performance purposes)
+    private double epsilon = 1.68584e-2;
     void Start() {
+        SDPos = transform.position;
         print("Press space to roll die");
     }
 
     void Update(){
-        if (turnPhase == 1) {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                diceRoll = Random.Range(1, 299);
-                Debug.Log(diceRoll);
-                turnPhase++;
-            }
-        }
-        if (turnPhase == 2) {
-            if (diceRoll > 0) {
-                movePlayer();
-            } else if(diceRoll == 0) {
-                print("no more turns, exiting");
-                turnPhase++;
-            }
+        switch (turnPhase) {
+            case 1:
+                if (Input.GetKeyDown(KeyCode.Space)) {
+                    diceRoll = Random.Range(1, 7);
+                    Debug.Log(diceRoll);
+                    turnPhase++;
+                }
+                break;
+            case 2:
+                if (diceRoll > 0 && canMove) {
+                    movePlayer();
+                } else if (diceRoll == 0) {
+                    turnPhase--;
+                }
+                break;
+            default:
+                break;
         }
     }
 
     void movePlayer() {
         // Arrow/WASD Control
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) && upTri.GetComponent<SpriteRenderer>().enabled) {
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && upTri.GetComponent<SpriteRenderer>().enabled) {
             //If the pressed key is not opposite of last direction
             if (lastPress != "down") {
                 //Set all but the opposite-side-triangle active
@@ -46,61 +72,74 @@ public class PlayerController : MonoBehaviour
                 leftTri.SetActive(true);
                 rightTri.SetActive(true);
                 downTri.SetActive(false);
-                //If the player is allowed to move in the direction, move one unit
-                if (upTri.GetComponent<SpriteRenderer>().enabled) {
-                    transform.Translate(0, 1, 0);
-                    //Dice decrement
-                    diceRoll--;
-                    print(diceRoll);
-                }
+
+                SDPos = upTri.GetComponent<AdjacentCheck>().tile.transform.position;
+                SDCheck = (transform.position.y < upTri.GetComponent<AdjacentCheck>().tile.transform.position.y);
+                //Dice decrement
+                diceRoll--;
+                print(diceRoll);
+
                 //Set the lastPress variable to the current direction
                 lastPress = "up";
-            } else print("Cannot move up"); //Debug
+            }
         }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow) && leftTri.GetComponent<SpriteRenderer>().enabled) {
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && leftTri.GetComponent<SpriteRenderer>().enabled) {
             if (lastPress != "right") {
                 upTri.SetActive(true);
                 leftTri.SetActive(true);
                 rightTri.SetActive(false);
                 downTri.SetActive(true);
 
-                if (leftTri.GetComponent<SpriteRenderer>().enabled) {
-                    transform.Translate(-1, 0, 0);
-                    diceRoll--;
-                    print(diceRoll);
-                }
+                SDPos = leftTri.GetComponent<AdjacentCheck>().tile.transform.position;
+                SDCheck = (transform.position.x > leftTri.GetComponent<AdjacentCheck>().tile.transform.position.x);
+                diceRoll--;
+                print(diceRoll);
+
                 lastPress = "left";
-            } else print("Cannot move left");
+            } 
         }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) && downTri.GetComponent<SpriteRenderer>().enabled) {
+        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && downTri.GetComponent<SpriteRenderer>().enabled) {
             if (lastPress != "up") {
                 upTri.SetActive(false);
                 leftTri.SetActive(true);
                 rightTri.SetActive(true);
                 downTri.SetActive(true);
 
-                if (downTri.GetComponent<SpriteRenderer>().enabled) {
-                    transform.Translate(0, -1, 0);
-                    diceRoll--;
-                    print(diceRoll);
-                }
+                SDPos = downTri.GetComponent<AdjacentCheck>().tile.transform.position;
+                SDCheck = (transform.position.y > downTri.GetComponent<AdjacentCheck>().tile.transform.position.y);
+                diceRoll--;
+                print(diceRoll);
+
                 lastPress = "down";
-            } else print("Cannot move down");
+            }
         }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow) && rightTri.GetComponent<SpriteRenderer>().enabled) {
+        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && rightTri.GetComponent<SpriteRenderer>().enabled) {
             if (lastPress != "left") {
                 upTri.SetActive(true);
                 leftTri.SetActive(false);
                 rightTri.SetActive(true);
                 downTri.SetActive(true);
 
-                if (rightTri.GetComponent<SpriteRenderer>().enabled) {
-                    transform.Translate(1, 0, 0);
-                    diceRoll--;
-                    print(diceRoll);
-                }
+                SDPos = rightTri.GetComponent<AdjacentCheck>().tile.transform.position;
+                SDCheck = (transform.position.x < rightTri.GetComponent<AdjacentCheck>().tile.transform.position.x);
+                diceRoll--;
+                print(diceRoll);
+                
                 lastPress = "right";
-            } else print("Cannot move right");
+            } 
+        }
+    }
+
+    void FixedUpdate() {
+        if (SDCheck) {
+            canMove = false;
+            transform.position = Vector2.SmoothDamp(transform.position, SDPos, ref vel, moveSpeed * Time.deltaTime);
+            if ((transform.position - SDPos).magnitude < epsilon) {
+                print("done");
+                canMove = true;
+                SDCheck = false;
+                transform.position = SDPos;
+            }
         }
     }
 }
